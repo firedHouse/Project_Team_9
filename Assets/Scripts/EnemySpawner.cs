5915ff16e40aef63077d39ebc7e785d6336a35f1
firedHouse <y.hyungu4704@gmail.com>
2025-10-29 01:11:29 +0900

    [feat] EnemySpawner 기능 개선
    NavMesh를 통해 적절한 위치에 생성되는 로직을 제작. 테스트 필요.

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,18 +1,109 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemySpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Enemy Prefabs")]
+    [SerializeField] private GameObject[] _enemyPrefabs;
+
+    //일단 맵 전체를 바운더리로 설정하여 스포너 하나만 사용해볼 예정
+    [Header("Spawner Boundary")]
+    [SerializeField] private Vector2 _mapMinXZ = new Vector2(-50f, -50f);
+    [SerializeField] private Vector2 _mapMaxXZ = new Vector2(50f, 50f);
+    [SerializeField] private float _navMeshSearchRange = 10f; // NavMesh 검색 반경
+
+    //스폰 주기만 있는데 뭔가 더 있는 게 좋을 듯
+    [Header("Spawn Settings")]
+    [SerializeField] private float _spawnInterval = 2.0f;
+    [SerializeField] private int _maxTotalEnemies = 50;
+
+    private Transform _playerTransform;
+
+    private void Start()
     {
-        
+        //플레이어 오브젝트 태그를 추적
+        GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+        if (playerObj != null)
+        {
+            _playerTransform = playerObj.transform;
+        }
+
+        //게임 상태 변경 구독!
+        GameManager.Instance.OnStateChanged += OnGameStateChanged;
+    }
+    private void OnDestroy()
+    {
+        if (GameManager.Instance != null)
+        {
+            //구독 해제
+            GameManager.Instance.OnStateChanged -= OnGameStateChanged;
+        }
+        CancelInvoke(nameof(SpawnEnemy));
     }
 
-    // Update is called once per frame
-    void Update()
+    //Play 여부에 따라 스폰 메서드 On, off 판단하는 메서드
+    private void OnGameStateChanged(GameManager.GameState newState)
     {
-        
+        if (newState == GameManager.GameState.Play)
+        {
+            //Play 상태가 되면 스폰 메서드를 _spawnInterval마다 호출
+            InvokeRepeating(nameof(SpawnEnemy), 0f, _spawnInterval);
+            Debug.Log("스폰 메서드 호출할거임");
+        }
+        else
+        {
+            //Play 아니면 반복 호출 끄기
+            CancelInvoke(nameof(SpawnEnemy));
+            Debug.Log("Enemy Spawner: InvokeRepeating 중지.");
+        }
+    }
+    //에너미 스폰 메서드
+    private void SpawnEnemy()
+    {
+        if (_playerTransform == null || _enemyPrefabs.Length == 0)
+        {
+            return;
+        }
+
+        Vector3 spawnPosition = GetSpawnPosition();
+
+        if (spawnPosition == Vector3.zero)
+        {
+            Debug.Log("스폰할 곳 없음, Bake 확인");
+            return;
+        }
+
+
+        //유니티 랜덤을 통해 랜덤 함수 생성, 처음 써보는 거라 확인 필요함!
+        GameObject randomEnemyPrefab = _enemyPrefabs[UnityEngine.Random.Range(0, _enemyPrefabs.Length)];
+        GameObject newEnemy = Instantiate(randomEnemyPrefab, spawnPosition, Quaternion.identity); //오브젝트 풀링 만들면 변경해야함
+
+        newEnemy.transform.position = spawnPosition;
+        newEnemy.SetActive(true);
+    }
+
+    //주변 오브젝트 체크용으로 네비매쉬 사용
+    private Vector3 GetSpawnPosition()
+    {
+        Vector3 finalPosition = Vector3.zero;
+        NavMeshHit hit;
+
+        //조건 체크항목인데 시간 되면 do while로 리팩토링 할 듯
+        for (int i = 0; i < 15; i++)
+        {
+            //경계 내 무작위 위치값 받아오고
+            float randomX = UnityEngine.Random.Range(_mapMinXZ.x, _mapMaxXZ.x);
+            float randomZ = UnityEngine.Random.Range(_mapMinXZ.y, _mapMaxXZ.y);
+            Vector3 randomPosition = new Vector3(randomX, 0f, randomZ);
+
+            if (NavMesh.SamplePosition(randomPosition, out hit, _navMeshSearchRange, NavMesh.AllAreas))
+            {
+                finalPosition = hit.position;
+                return finalPosition;
+            }
+        }
+        return Vector3.zero;
     }
 }
