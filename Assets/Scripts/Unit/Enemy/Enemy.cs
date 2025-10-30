deff337f542ef53f1a0e3c45e50b2be0b92d67ec
firedHouse <y.hyungu4704@gmail.com>
2025-10-30 15:56:53 +0900

    [feat] Enemy 데미지 로직 추가
    닿을 시 일정 시간마다 데미지 입히는 로직

--- Assets/Scripts/Unit/Enemy/Enemy.cs
+++ Assets/Scripts/Unit/Enemy/Enemy.cs
@@ -8,10 +8,13 @@ public class Enemy : Unit
 {
     //반납할 때 사용할 키 = 프리펩의 이름
     [Header("Pool Key")]
-    [SerializeField] private string _prefabKey = "EnemyPrefabName"; 
+    [SerializeField] private string _prefabKey = "EnemyPrefabName";
 
     // 어택 세팅 필요
-    [SerializeField] private float attackDamage = 10f;
+    [SerializeField] private float _attackDamage = 5f;
+    [SerializeField] private float _attackCooldown = 1.0f;
+    private float _lastAttackTime = 0f;
+
 
     private float _lastUpdateTime = 0f;
     private float _DestinationInterval = 3f;
@@ -45,7 +48,7 @@ public class Enemy : Unit
         {
             navMeshAgent.enabled = true;
             navMeshAgent.speed = moveSpeed;
-            
+            currentHp = maxHp;
         }
     }
 
@@ -64,15 +67,18 @@ public class Enemy : Unit
         }
     }
 
+    //여현구: 어떤 컴포넌트를 플레이어 오브젝트에 담을 지에 따라 GetComponent가 달라짐 유닛으로 통일
     private void OnCollisionStay(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            //여현구: 어떤 컴포넌트를 플레이어 오브젝트에 담을 지에 따라 GetComponent가 달라짐. Player, Unit, Job 등등
-            Unit playerUnit = collision.gameObject.GetComponent<Unit>();
-            playerUnit.TakeDamage(attackDamage);
-
-            Debug.Log("플레이어와 충돌하여 데미지");
+            if (Time.time >= _lastAttackTime + _attackCooldown)
+            {
+                Unit playerUnit = collision.gameObject.GetComponent<Unit>();
+                playerUnit.TakeDamage(_attackDamage);
+                Debug.Log("플레이어와 충돌하여 데미지");
+                _lastAttackTime = Time.time;
+            }
 
         }
     }
