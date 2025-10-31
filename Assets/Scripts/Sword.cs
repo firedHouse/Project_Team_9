158807951e10069e21d1265fddffd5e18f65ad67
Hobread0202 <yihosung1@gmail.com>
2025-10-31 16:20:34 +0900

    [Add] Knigt, Archer ActiveSkill 추가
    w 직업 스킬

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -5,26 +5,31 @@ using UnityEngine;
 public class Sword : MonoBehaviour
 {
     private Collider swordCollider;
+    private Player player;
     private void Awake()
     {
         swordCollider = GetComponent<Collider>();
         swordCollider.enabled = false;
+
     }
 
     private void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Q))
+        if (Input.GetKeyUp(KeyCode.Q) || Input.GetKeyDown(KeyCode.W))
         {
             StartCoroutine(ActivateCollider());
         }
+
     }
 
     private IEnumerator ActivateCollider()
-    {
-        yield return new WaitForSeconds(1f);
+    {   
+
+        yield return new WaitForSeconds(0.5f);
         swordCollider.enabled = true;
         yield return new WaitForSeconds(0.5f);
         swordCollider.enabled = false;
+
     }
 
     private void OnTriggerEnter(Collider other)
