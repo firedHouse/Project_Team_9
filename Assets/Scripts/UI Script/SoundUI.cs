bf2b84a5b8f1647a5a44c3dd6b06af558d80ed07
ChocoCider <hsj183040@gmail.com>
2025-10-31 16:19:51 +0900

    [Add] SoundUI
    [Add] SoundUI
    [Edit] OptionUI - Sound Slider Add

---
+++ Assets/Scripts/UI Script/SoundUI.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundUI : MonoBehaviour
+{
+    [SerializeField] AudioBehaviour audioBehaviour;
+    [SerializeField] private Slider MainSound;
+    [SerializeField] private Slider EffectSound;
+    [SerializeField] private Slider MusicSound;
+
+    
+}
